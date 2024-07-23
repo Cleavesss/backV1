f8576b741645bbd69c73e183ec707a943a611207
Смородинов Илья Константинович <cleavessss21dgtv@mail.ru>
2024-07-23 22:10:14 +0400

    работающий локально постгрес

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { UserModule } from "./users/users.module";
 import { ConfigModule } from "@nestjs/config";
-import { User } from "./users/user.module";
+import { User } from "./users/users.model";
 
 @Module({
     controllers: [],
