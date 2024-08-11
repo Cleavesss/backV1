80e597ebf1c2bd86be767d90ba62d976bd0c50f5
Смородинов Илья Константинович <cleavessss21dgtv@mail.ru>
2024-08-11 19:55:57 +0400

    httpCode 200 login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { ApiProperty, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
@@ -17,6 +17,7 @@ export class AuthController {
     @ApiOperation({summary: 'Аутентификация пользователя'})
     @ApiResponse({status: 200, description: 'Вовзращается access JWT Token', type: TResponse})
     @Post('/login')
+    @HttpCode(200)
     login(@Body() userDto: CreateUserDto){
         return this.authService.login(userDto)
     }
